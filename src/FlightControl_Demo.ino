ab853fc06c4da6a00c7426e48b3aeecd29e25020
Bobby Schulz <schulz.bobby@gmail.com>
2022-06-23 12:40:46 -0500

    Added global pin file, updated main

--- src/FlightControl_Demo.ino
+++ src/FlightControl_Demo.ino
@@ -9,22 +9,30 @@
 #include <PCAL9535A.h>
 #include <Sensor.h>
 #include <Talon.h>
+#include <Kestrel.h>
+#include <KestrelFileHandler.h>
 #include <vector>
 #include <memory>
 
 const String firmwareVersion = "1.0.1";
 
-AuxTalon aux(4, 0x14); //Instantiate AUX talon with deaults - port 4 and hardware v1.4
-AuxTalon aux1(3, 0x14); //Instantiate AUX talon with alt - port 3 and hardware v1.4
+Kestrel logger;
+KestrelFileHandler fileSys;
+AuxTalon aux(0, 0x14); //Instantiate AUX talon with deaults - port 4 and hardware v1.4
+AuxTalon aux1(0, 0x14); //Instantiate AUX talon with alt - port 3 and hardware v1.4
 PCAL9535A ioAlpha(0x20);
 PCAL9535A ioBeta(0x21);
 
 const uint8_t numSensors = 2; 
+const uint8_t numTalons = 2;
 // std::vector<std::unique_ptr<Sensor>> sensors;
 Sensor* const sensors[numSensors] = {
 	&aux,
 	&aux1
 };
+
+Talon* talons[Kestrel::numTalonPorts]; //Create an array of the total possible length
+
 // sensors.push_back(aux);
 // sensors.push_back(aux1);
 // sensors.emlace_back(aux);
@@ -32,19 +40,19 @@ Sensor* const sensors[numSensors] = {
 
 // Sensor sensors[2] = {aux, aux1};
 
-namespace Pins { //Use for B402
-	constexpr uint16_t WD_HOLD  = D2;
-	constexpr uint16_t SD_CS    = D8;
-	constexpr uint16_t Clock_INT 	= D22;
-	constexpr uint16_t TALON1_GPIOA = A3;
-	constexpr uint16_t TALON1_GPIOB = D7;
-	constexpr uint16_t TALON2_GPIOA = A2;
-	constexpr uint16_t TALON2_GPIOB = D6;
-	constexpr uint16_t TALON3_GPIOA = A1;
-	constexpr uint16_t TALON3_GPIOB = D5;
-	constexpr uint16_t I2C_GLOBAL_EN = D23; //FIX!
-	constexpr uint16_t I2C_OB_EN = A6; //FIX!
-}
+// namespace Pins { //Use for B402
+// 	constexpr uint16_t WD_HOLD  = D2;
+// 	constexpr uint16_t SD_CS    = D8;
+// 	constexpr uint16_t Clock_INT 	= D22;
+// 	constexpr uint16_t TALON1_GPIOA = A3;
+// 	constexpr uint16_t TALON1_GPIOB = D7;
+// 	constexpr uint16_t TALON2_GPIOA = A2;
+// 	constexpr uint16_t TALON2_GPIOB = D6;
+// 	constexpr uint16_t TALON3_GPIOA = A1;
+// 	constexpr uint16_t TALON3_GPIOB = D5;
+// 	constexpr uint16_t I2C_GLOBAL_EN = D23; //FIX!
+// 	constexpr uint16_t I2C_OB_EN = A6; //FIX!
+// }
 
 namespace PinsIO { //For Kestrel v1.1
 	constexpr uint16_t VUSB = 5;
@@ -82,33 +90,87 @@ SYSTEM_MODE(AUTOMATIC);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
+	//////////// MANUAL POSITIONING //////////////////
+  	// talons[aux.getTalonPort()] = &aux; //Place talon objects at coresponding positions in array
+	// talons[aux1.getTalonPort()] = &aux1;
+	
   Serial.begin(115200); 
-  I2C_OnBoardEn(true); 
+  waitFor(Serial.isConnected, 30000);
+  logger.begin();
+  logger.enableI2C_OB(true);
+
+	//FIX! Turn on correct things before running file begin
+  fileSys.begin();
+
+//   I2C_OnBoardEn(true); 
 	Wire.setClock(400000); //Confirm operation in fast mode
 	Wire.begin();
 	ioAlpha.begin(); //RESTORE
 	ioBeta.begin(); //RESTORE
 
-  I2C_OnBoardEn(true); //Turn on internal I2C
-  ioBeta.pinMode(PinsIOBeta::EN4, OUTPUT);
-  ioBeta.pinMode(PinsIOBeta::I2C_EN4, OUTPUT);
-  ioBeta.pinMode(PinsIOBeta::SEL4, OUTPUT);
+//   I2C_OnBoardEn(true); //Turn on internal I2C
+//   ioBeta.pinMode(PinsIOBeta::EN4, OUTPUT);
+//   ioBeta.pinMode(PinsIOBeta::I2C_EN4, OUTPUT);
+//   ioBeta.pinMode(PinsIOBeta::SEL4, OUTPUT);
 
-  ioBeta.digitalWrite(PinsIOBeta::EN4, HIGH); //Turn on power to port 4
-  ioBeta.digitalWrite(PinsIOBeta::I2C_EN4, HIGH); //Turn on I2C to port 4
-  ioBeta.digitalWrite(PinsIOBeta::SEL4, LOW); //Turn on GPIO connection to port 4
+//   ioBeta.digitalWrite(PinsIOBeta::EN4, HIGH); //Turn on power to port 4
+//   ioBeta.digitalWrite(PinsIOBeta::I2C_EN4, HIGH); //Turn on I2C to port 4
+//   ioBeta.digitalWrite(PinsIOBeta::SEL4, LOW); //Turn on GPIO connection to port 4
   ////////// ADD INTERRUPTS!
+  	// logger.enablePower(3, true);
+	for(int i = 1; i <= Kestrel::numTalonPorts; i++) { //Iterate over ALL ports
+		logger.enablePower(i, true); //Turn on all power by default
+		logger.enableData(i, false); //Turn off all data by default
+	}
+	
+	// logger.enableData(3, true);
+	// logger.enableI2C_OB(false);
+	// logger.enableI2C_Global(true);
+	////////////// AUTO TALON DETECTION ///////////////////////
+	talons[0] = &aux; //Place talon objects at arbitrary positions in array
+	talons[1] = &aux1;
+	for(int port = 1; port <= Kestrel::numTalonPorts; port++) { //Test all ports
+		logger.enableData(port, true); //Turn on specific channel
+		for(int t = 0; t < numTalons; t++) { //Iterate over all Talon objects
+			if(talons[t]->getTalonPort() == 0) { //If port not already specified 
+				logger.enableI2C_Global(true);
+				logger.enableI2C_OB(false);
+				if(talons[t]->isPresent()) { //Test if that Talon is present, if it is, configure the port
+					talons[t]->setTalonPort(port);
+					Serial.print("Talon Port Result "); //DEBUG!
+					Serial.print(t);
+					Serial.print(": ");
+					Serial.println(talons[t]->getTalonPort());
+					break; //Exit the interation after the first one tests positive 
+				}
+			}
+			
+		}
+		logger.enableData(port, false); //Turn port back off
+	}
+	talons[aux.getTalonPort() - 1] = &aux; //Place talon objects at detected positions in array
+	talons[aux1.getTalonPort() - 1] = &aux1; 
 
-  I2C_OnBoardEn(false);
-  I2C_GlobalEn(true);
+//   I2C_OnBoardEn(false);	
+//   I2C_GlobalEn(true);
 
-	bool hasCriticalError = false;
-	bool hasError = false;
-  String initDiagnostic = aux.begin(Time.now(), hasCriticalError, hasError);
+// 	bool hasCriticalError = false;
+// 	bool hasError = false;
+// 	logger.enableData(4, true);
+// 	logger.enableI2C_OB(false);
+// 	logger.enableI2C_Global(true);
+//   String initDiagnostic = aux.begin(Time.now(), hasCriticalError, hasError);
+
+	String initDiagnostic = initSensors();
 	Serial.print("DIAGNOSTIC: ");
 	Serial.println(initDiagnostic);
-	Particle.publish("DIAGNOSTIC", initDiagnostic);
+	fileSys.writeToParticle(initDiagnostic, "diagnostic"); 
+	// Particle.publish("diagnostic", initDiagnostic);
+
+	// logger.enableData(3, true);
+	// logger.enableI2C_OB(false);
+	// logger.enableI2C_Global(true);
+	// aux1.begin(Time.now(), hasCriticalError, hasError);
   //FIX! RESPOND TO ERROR RESULTS! 
 }
 
@@ -124,10 +186,10 @@ void loop() {
   String metadata = getMetadataString();
 	String data = getDataString();
 
-  Particle.publish("DIAGNOSTIC", diagnostic);
-  Particle.publish("ERROR", errors);
-  Particle.publish("DATA", data);
-  Particle.publish("METADATA", metadata);
+  Particle.publish("diagnostic", diagnostic);
+  Particle.publish("error", errors);
+  Particle.publish("data", data);
+  Particle.publish("metadata", metadata);
 
   Serial.print("DIAGNOSTIC: ");
   Serial.println(diagnostic);
@@ -143,17 +205,17 @@ void loop() {
 
 }
 
-void I2C_GlobalEn(bool State)
-{
-	pinMode(Pins::I2C_GLOBAL_EN, OUTPUT);
-	digitalWrite(Pins::I2C_GLOBAL_EN, State);
-}
+// void I2C_GlobalEn(bool State)
+// {
+// 	pinMode(Pins::I2C_GLOBAL_EN, OUTPUT);
+// 	digitalWrite(Pins::I2C_GLOBAL_EN, State);
+// }
 
-void I2C_OnBoardEn(bool State)
-{
-	pinMode(Pins::I2C_OB_EN, OUTPUT);
-	digitalWrite(Pins::I2C_OB_EN, State);
-}
+// void I2C_OnBoardEn(bool State)
+// {
+// 	pinMode(Pins::I2C_OB_EN, OUTPUT);
+// 	digitalWrite(Pins::I2C_OB_EN, State);
+// }
 
 String getDataString()
 {
@@ -186,6 +248,24 @@ String getDiagnosticString(uint8_t level)
 		// data = data + "\"Devices\":[" + sensors[0]->getData(Time.now()) + "," + sensors[1]->getData(Time.now()) + "]}}";
 		diagnostic = diagnostic + "\"Devices\":[";
 		for(int i = 0; i < numSensors; i++) {
+			logger.disableDataAll(); //Turn off data to all ports, then just enable those needed
+			Serial.print("Talon Port "); //DEBUG!
+			Serial.print(i);
+			Serial.print(": ");
+			Serial.println(sensors[i]->getTalonPort());
+			Serial.flush();
+			logger.enableData(sensors[i]->getTalonPort(), true); //Turn on data to required Talon port
+			// if(!sensors[i]->isTalon()) { //If sensor is not Talon
+			if(sensors[i]->getTalonPort() > 0) { //If a Talon is associated with the sensor, turn that port on
+				talons[sensors[i]->getTalonPort() - 1]->disableDataAll(); //Turn off all data on Talon
+				talons[sensors[i]->getTalonPort() - 1]->enableData(sensors[i]->getSensorPort(), true); //Turn back on only port used
+			}
+			
+			// }
+			// logger.enablePower(sensors[i]->getTalon(), true); //Turn on power to port
+			// logger.enableData(sensors[i]->getTalon(), true); //Turn on data to port
+			logger.enableI2C_OB(false);
+			logger.enableI2C_Global(true);
 			diagnostic = diagnostic + sensors[i]->selfDiagnostic(level, Time.now());
 			if(i + 1 < numSensors) diagnostic = diagnostic + ","; //Only append if not last entry
 		}
@@ -212,9 +292,21 @@ String getMetadataString()
 		// metadata = metadata + "\"Devices\":[" + sensors[0]->getMetadata() + "," + sensors[1]->getMetadata() + "]}}";
 	metadata = metadata + "\"Devices\":[";
 	for(int i = 0; i < numSensors; i++) {
+		logger.disableDataAll(); //Turn off data to all ports, then just enable those needed
+		logger.enableData(sensors[i]->getTalonPort(), true); //Turn on data to required Talon port
+			// if(!sensors[i]->isTalon()) { //If sensor is not Talon
+		if(sensors[i]->getTalonPort() > 0) { //If a Talon is associated with the sensor, turn that port on
+			talons[sensors[i]->getTalonPort() - 1]->disableDataAll(); //Turn off all data on Talon
+			talons[sensors[i]->getTalonPort() - 1]->enableData(sensors[i]->getSensorPort(), true); //Turn back on only port used
+		}
+		// logger.enablePower(sensors[i]->getTalon(), true); //Turn on power to port
+		// logger.enableData(sensors[i]->getTalon(), true); //Turn on data to port
+		logger.enableI2C_OB(false);
+		logger.enableI2C_Global(true);
 		metadata = metadata + sensors[i]->getMetadata();
 		if(i + 1 < numSensors) metadata = metadata + ","; //Only append if not last entry
 	}
+
 	metadata = metadata + "]}}"; //Close metadata
 	//   }
 	//   for (auto &device: sensors) {
@@ -222,4 +314,47 @@ String getMetadataString()
 	//   }
 	//   String data = aux.getData(Time.now());
 	return metadata;
+}
+
+String initSensors()
+{
+	String leader = "{\"Diagnostic\":{";
+	leader = leader + "\"Time\":" + String(Time.now()) + ","; //Concatonate time
+	leader = leader + "\"NumDevices\":" + String(numSensors) + ",\"Devices\":["; //Concatonate number of sensors 
+	
+	String closer = "]}}";
+	String output = leader;
+
+	// output = output + "\"Devices\":[";
+	for(int i = 0; i < numSensors; i++) {
+		logger.disableDataAll(); //Turn off data to all ports, then just enable those needed
+		logger.enableData(sensors[i]->getTalonPort(), true); //Turn on data to required Talon port
+		// if(!sensors[i]->isTalon()) { //If sensor is not Talon
+		if(sensors[i]->getTalonPort() > 0) { //If a Talon is associated with the sensor, turn that port on
+			talons[sensors[i]->getTalonPort() - 1]->disableDataAll(); //Turn off all data on Talon
+			talons[sensors[i]->getTalonPort() - 1]->enableData(sensors[i]->getSensorPort(), true); //Turn back on only port used
+		}
+		
+		// }
+		// logger.enablePower(sensors[i]->getTalon(), true); //Turn on power to port
+		// logger.enableData(sensors[i]->getTalon(), true); //Turn on data to port
+		logger.enableI2C_OB(false);
+		logger.enableI2C_Global(true);
+		bool hasCriticalError = false;
+		bool hasError = false;
+
+  		String initDiagnostic = sensors[i]->begin(Time.now(), hasCriticalError, hasError);
+		if(output.length() - output.lastIndexOf('\n') + initDiagnostic.length() + closer.length() + 1 < Kestrel::MAX_MESSAGE_LENGTH) { //Add +1 to account for comma appending, subtract any previous lines from count
+			if(i > 0) output = output + ","; //Add preceeding comma if not the first entry
+			output = output + initDiagnostic; //Append result 
+			// if(i + 1 < numSensors) diagnostic = diagnostic + ","; //Only append if not last entry
+		}
+		else {
+			output = output + closer + "\n"; //End this packet
+			output = output + leader + initDiagnostic; //Start a new packet and add new payload 
+		}
+		
+	}
+	output = output + "]}}"; //Close diagnostic
+	return output;
 }
---
+++ src/GlobalPins.h
@@ -0,0 +1,38 @@
+#ifndef GlobalPins_h
+#define GlobalPins_h
+
+// namespace KestrelPins { //Use for B402
+// 	constexpr uint16_t WD_HOLD  = D2;
+// 	constexpr uint16_t SD_CS    = D8;
+// 	constexpr uint16_t Clock_INT 	= D22;
+// 	constexpr uint16_t TALON1_GPIOA = A3;
+// 	constexpr uint16_t TALON1_GPIOB = D7;
+// 	constexpr uint16_t TALON2_GPIOA = A2;
+// 	constexpr uint16_t TALON2_GPIOB = D6;
+// 	constexpr uint16_t TALON3_GPIOA = A1;
+// 	constexpr uint16_t TALON3_GPIOB = D5;
+// 	constexpr uint16_t I2C_GLOBAL_EN = D23; //FIX!
+// 	constexpr uint16_t I2C_OB_EN = A6; //FIX!
+// }
+namespace KestrelPins {
+	constexpr uint8_t PortAPins[4] = {A3, A2, A1, A0};
+	constexpr uint8_t PortBPins[4] = {D7, D6, D5, D4};
+}
+
+// class KestrelPins
+// {
+// 	public:
+// 		// Talon();
+// 		// const uint8_t PortAPins[4] = {A3, A2, A1, A0};
+// 		// const uint8_t PortBPins[4] = {D7, D6, D5, D4};
+// 		// int restart();
+// 		// virtual int enableData(uint8_t Port){
+// 		// 	return 0;
+// 		// };
+// 		// virtual int enablePower(uint8_t Port){
+// 		// 	return 0;
+// 		// };
+
+// };
+
+#endif

537e223c0ec7b0d876ba2c6ae421ed020bf7bbab
Bobby Schulz <schulz.bobby@gmail.com>
2022-06-23 12:48:19 -0500

    Updated libs

--- lib/Sensor/src/Sensor.h
+++ lib/Sensor/src/Sensor.h
@@ -6,6 +6,7 @@ class Sensor
 
 	public:
 		// Sensor();
+		virtual String begin(time_t time, bool &criticalFault, bool &fault);
 		virtual String getData(time_t time) {
 			return ""; //Return empty string if function is not implemented by device 
 		};
@@ -15,6 +16,20 @@ class Sensor
 		virtual String selfDiagnostic(uint8_t diagnosticLevel, time_t time) {
 			return ""; //return empty string if function is not implemented by device 
 		};
+		// virtual bool isTalon() {
+		// 	return false; //Default to not
+		// };
+		virtual uint8_t getTalonPort() {
+			return 0; //DEBUG!
+		};
+		virtual uint8_t getSensorPort() {
+			return 0; //DEBUG!
+		};
+		virtual void setTalonPort(uint8_t port){};
+		virtual void setSensorPort(uint8_t pos){};
+		virtual bool isPresent() {
+			return false;
+		}; 
 };
 
 #endif
--- lib/Talon/src/Talon.h
+++ lib/Talon/src/Talon.h
@@ -6,12 +6,31 @@ class Talon
 	public:
 		// Talon();
 		int restart();
-		virtual int enableData(uint8_t Port){
+		virtual int enableData(uint8_t Port, bool state){
 			return 0;
 		};
-		virtual int enablePower(uint8_t Port){
+		virtual int enablePower(uint8_t Port, bool state){
 			return 0;
 		};
+		int disableDataAll(){
+			// for(int i = 0; i < 4; i++) {
+			// 	enableData(i, false);
+			// }
+			return 0;
+		};
+		int disablePowerAll(){
+			// for(int i = 0; i < numPorts; i++) {
+			// 	enablePower(i, false);
+			// }
+			return 0;
+		};
+		virtual uint8_t getTalonPort() {
+			return 0; //DEBUG!
+		};
+		virtual void setTalonPort(uint8_t port_);
+		virtual bool isPresent();
+		// virtual uint8_t getPort();
+		// virtual void setPort(uint8_t port);
 
 };
 

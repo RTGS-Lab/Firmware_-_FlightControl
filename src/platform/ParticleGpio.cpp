d5853140bfeebdd607f9a299ad27c4b76ae8ee30
Zach Radlicz <zradlicz@gmail.com>
2025-03-31 16:48:10 -0500

    started getting rid of gpio dependency

---
+++ src/platform/ParticleGpio.cpp
@@ -0,0 +1,5 @@
+#include "ParticleGpio.h"
+
+void ParticleGpio::pinMode(uint16_t pin, PinMode mode){::pinMode(uint16_t pin, PinMode mode);}
+void ParticleGpio::digitalWrite(uint16_t pin, uint8_t value){::digitalWrite(uint16_t pin, uint8_t value);}
+uint32_t ParticleGpio::digitalRead(uint16_t pin){::digitalRead(pin);}
---
+++ src/platform/ParticleGpio.h
@@ -0,0 +1,23 @@
+// src/platform/ParticleGpio.h
+#ifndef PARTICLE_GPIO_H
+#define PARTICLE_GPIO_H
+
+#include "IGpio.h" // Include the interface definition
+#include "Particle.h" // Include the actual Particle header HERE
+
+/**
+ * @brief Concrete implementation of IGpio
+ */
+class ParticleGpio : public IGpio {
+public:
+    // Constructor/Destructor (often default is fine)
+    ParticleGpio() = default;
+    ~ParticleGpio() override = default;
+
+    // Implement methods from IGpio
+    void pinMode(uint16_t pin, PinMode mode) override;
+    void digitalWrite(uint16_t pin, uint8_t value) override;
+    uint32_t digitlaRead(uint16_t pin) override;
+};
+
+#endif // PARTICLE_GPIO_H
